1a25e8db350caf180cb1755ef429a44e8ceebd0b
Lewap <lewap02@gmail.com>
2021-12-28 17:40:07 +0100

    added int

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World32");
+        int number = 5;
+        System.out.println(number);
     }
 }
